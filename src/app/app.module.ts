53d0669914f361083f30af75e6bb027ac236a72d
AAGuedes <airam.a.g.85@gmail.com>
2023-04-15 21:52:11 +0100

    Creado counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CounterComponent } from './counter/counter.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
