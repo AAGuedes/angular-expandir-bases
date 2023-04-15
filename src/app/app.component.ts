1dbe0b27edf4e53dd66dfeafa6110a4d3883ff15
AAGuedes <airam.a.g.85@gmail.com>
2023-04-15 21:40:41 +0100

    Creación del proyecto y agregado contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Mi primera App de Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter = this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
