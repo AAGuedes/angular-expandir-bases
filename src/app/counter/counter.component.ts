d9937c24cabc8ccdf85f0df4fa7ed30db0ca821d
AAGuedes <airam.a.g.85@gmail.com>
2023-04-15 21:59:40 +0100

    Movido el código del counter desde app.component a counter.component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>'
+  templateUrl: './counter.component.html'
 })
-export class CounterComponent {}
+export class CounterComponent {
+
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter = this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+
+}
